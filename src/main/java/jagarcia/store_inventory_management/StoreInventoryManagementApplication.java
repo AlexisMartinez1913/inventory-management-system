854f01f8b2fff3f7366d0d6e04e89601a34d71af
Alexis Martinez <jhonyalexis100@gmail.com>
2024-06-24 13:31:52 -0500

    init project

---
+++ src/main/java/jagarcia/store_inventory_management/StoreInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package jagarcia.store_inventory_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jagarcia/store_inventory_management/StoreInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package jagarcia.store_inventory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
