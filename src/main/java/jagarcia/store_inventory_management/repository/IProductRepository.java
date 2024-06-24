f01bb154816e200af508d7c3661dc3d250751cfd
Alexis Martinez <jhonyalexis100@gmail.com>
2024-06-24 13:57:18 -0500

    add service and impl

---
+++ src/main/java/jagarcia/store_inventory_management/repository/IProductRepository.java
@@ -0,0 +1,2 @@
+package jagarcia.store_inventory_management.repository;public interface IProductRepository {
+}
---
+++ src/main/java/jagarcia/store_inventory_management/service/impl/ProductServiceImpl.java
@@ -0,0 +1,2 @@
+package jagarcia.store_inventory_management.service.impl;public class ProductServiceImpl {
+}
